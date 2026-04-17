64b0921fb0ae49f2969a53971623e0f3b246e19e
Sakib3602 <sakibsarker6969@gmail.com>
2026-04-17 15:05:08 +0600

    added end logic to show end campaign

--- src/controllers/Marketing/campaignController.ts
+++ src/controllers/Marketing/campaignController.ts
@@ -12,3 +12,27 @@ export const createCampaign = async (req: Request, res: Response) => {
         res.status(500).json({ message: "Error creating campaign", error });
     }
 }
+
+export const allCampaign = async(req:   Request, res: Response) => {
+    try{
+        const {id} = req.params as {id: string}; // eita ashbo marketer er id
+        const campaigns = await Campaign.find({marketerId: id}).populate("marketerId", "name");
+        res.status(200).json(campaigns);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching campaigns", error });
+    }
+
+}
+
+export const campaignDone = async(req: Request, res: Response) => {
+    try{
+        const {id} = req.params as {id: string}; // eita ashbo marketing er id
+        const today = new Date();
+        console.log("today date:", today);
+
+        const campaigns = await Campaign.find({ marketerId: id, endDate: { $lte: today } })
+        res.status(200).json(campaigns);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching completed campaigns", error });
+    }
+}
--- src/routes/Marketer/campaign.ts
+++ src/routes/Marketer/campaign.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createCampaign } from "../../controllers/Marketing/campaignController.js";
+import { allCampaign, campaignDone, createCampaign } from "../../controllers/Marketing/campaignController.js";
 
 const router = Router();
 
 router.post('/create-campaign', createCampaign);
-
+router.get('/all-campaigns/:id', allCampaign);
+router.get('/completed-campaigns/:id', campaignDone);
 
 export default router;
